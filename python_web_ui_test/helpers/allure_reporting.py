fe35aaa1f2945b87e435afb2b4495865d105f04c
Iakiv Kramarenko <yashaka@gmail.com>
2021-06-16 19:53:02 +0300

    ups, found one more bug in own code:) in allure helper)

--- python_web_ui_test/helpers/allure_reporting.py
+++ python_web_ui_test/helpers/allure_reporting.py
@@ -8,6 +8,6 @@ def step(title):
         fn_with_logging_to_allure()
     else:
         def decorator(fn):
-            fn_with_logging_to_allure = Step(fn)
+            fn_with_logging_to_allure = Step(title)(fn)
             fn_with_logging_to_allure()
         return decorator
