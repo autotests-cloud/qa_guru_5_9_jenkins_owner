016699659183d3ee1b0810124f0076c31a50f6db
Iakiv Kramarenko <yashaka@gmail.com>
2021-06-16 14:57:13 +0300

    ; are not needed and redundant

--- tests/StudentRegistrationFormTests.py
+++ tests/StudentRegistrationFormTests.py
@@ -3,11 +3,11 @@ from selene import by, have, be
 from selene import browser
 from selene.support.shared.jquery_style import s, ss
 
-# import static io.qameta.allure.Allure.step;
+# import static io.qameta.allure.Allure.step
 
-class StudentRegistrationFormTests {
+class StudentRegistrationFormTests:
 
-    Faker faker = new Faker();
+    Faker faker = new Faker()
 
     String firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
@@ -25,132 +25,131 @@ class StudentRegistrationFormTests {
             picture = "1.png",
             currentAddress = faker.address().fullAddress(),
             state = "Uttar Pradesh",
-            city = "Merrut";
+            city = "Merrut"
 
     @Test
-    void successfulFillFormTest() {
-        step("Open students registration form", () -> {
-            open("https://demoqa.com/automation-practice-form");
-            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        });
+    void successfulFillFormTest() -> :
+        step("Open students registration form", () -> :
+            open("https://demoqa.com/automation-practice-form")
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"))
+        )
 
-        step("Fill students registration form", () -> {
-            step("Fill common data", () -> {
-                $("#firstName").val(firstName);
-                $("#lastName").val(lastName);
-                $("#userEmail").val(email);
-                $("#genterWrapper").$(byText(gender)).click();
-                $("#userNumber").val(mobile);
-            });
-            step("Set date", () -> {
-                $("#dateOfBirthInput").clear();
-                $(".react-datepicker__month-select").selectOption(monthOfBirth);
-                $(".react-datepicker__year-select").selectOption(yearOfBirth);
-                $(".react-datepicker__day--0" + dayOfBirth).click();
-            });
-            step("Set subjects", () -> {
-                $("#subjectsInput").val(subject1);
-                $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
-                $("#subjectsInput").val(subject2);
-                $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
-            });
-            step("Set hobbies", () -> {
-                $("#hobbiesWrapper").$(byText(hobby1)).click();
-                $("#hobbiesWrapper").$(byText(hobby2)).click();
-                $("#hobbiesWrapper").$(byText(hobby3)).click();
-            });
+        step("Fill students registration form", () -> :
+            step("Fill common data", () -> :
+                $("#firstName").val(firstName)
+                $("#lastName").val(lastName)
+                $("#userEmail").val(email)
+                $("#genterWrapper").$(byText(gender)).click()
+                $("#userNumber").val(mobile)
+            )
+            step("Set date", () -> :
+                $("#dateOfBirthInput").clear()
+                $(".react-datepicker__month-select").selectOption(monthOfBirth)
+                $(".react-datepicker__year-select").selectOption(yearOfBirth)
+                $(".react-datepicker__day--0" + dayOfBirth).click()
+            )
+            step("Set subjects", () -> :
+                $("#subjectsInput").val(subject1)
+                $(".subjects-auto-complete__menu-list").$(byText(subject1)).click()
+                $("#subjectsInput").val(subject2)
+                $(".subjects-auto-complete__menu-list").$(byText(subject2)).click()
+            )
+            step("Set hobbies", () -> :
+                $("#hobbiesWrapper").$(byText(hobby1)).click()
+                $("#hobbiesWrapper").$(byText(hobby2)).click()
+                $("#hobbiesWrapper").$(byText(hobby3)).click()
+            )
             // TODO: consider refactor code like below for same style as everywhere else for consistency/easier reading
             /*
-            step("Upload image", () -> {
+            step("Upload image", () -> :
                 $("#uploadPicture").uploadFromClasspath("img/" + picture)
-            });
+            )
              */
             step("Upload image", () ->
-                    $("#uploadPicture").uploadFromClasspath("img/" + picture));
-            step("Set address", () -> {
-                $("#currentAddress").val(currentAddress);
-                $("#state").scrollTo().click();
-                $("#stateCity-wrapper").$(byText(state)).click();
-                $("#city").click();
-                $("#stateCity-wrapper").$(byText(city)).click();
-            });
+                    $("#uploadPicture").uploadFromClasspath("img/" + picture))
+            step("Set address", () -> :
+                $("#currentAddress").val(currentAddress)
+                $("#state").scrollTo().click()
+                $("#stateCity-wrapper").$(byText(state)).click()
+                $("#city").click()
+                $("#stateCity-wrapper").$(byText(city)).click()
+            )
             step("Submit form", () ->
-                $("#submit").click());
-        });
+                $("#submit").click())
+        )
 
-        step("Verify successful form submit", () -> {
-            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
-            $x("//td[text()='Student Email']").parent().shouldHave(text(email));
-            $x("//td[text()='Gender']").parent().shouldHave(text(gender));
-            $x("//td[text()='Mobile']").parent().shouldHave(text(mobile));
-            $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
-            $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
-            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
-            $x("//td[text()='Picture']").parent().shouldHave(text(picture));
-            $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
-            $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
-        });
-    }
+        step("Verify successful form submit", () -> :
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"))
+            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName))
+            $x("//td[text()='Student Email']").parent().shouldHave(text(email))
+            $x("//td[text()='Gender']").parent().shouldHave(text(gender))
+            $x("//td[text()='Mobile']").parent().shouldHave(text(mobile))
+            $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth))
+            $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2))
+            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3))
+            $x("//td[text()='Picture']").parent().shouldHave(text(picture))
+            $x("//td[text()='Address']").parent().shouldHave(text(currentAddress))
+            $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city))
+        )
+    
 
     @Test
     void negativeFillFormTest() {
-        step("Open students registration form", () -> {
-            open("https://demoqa.com/automation-practice-form");
-            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        });
+        step("Open students registration form", () -> :
+            open("https://demoqa.com/automation-practice-form")
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"))
+        )
 
-        step("Fill students registration form", () -> {
-            step("Fill common data", () -> {
-                $("#firstName").val(firstName);
-                $("#lastName").val(lastName);
-                $("#userEmail").val(email);
-                $("#genterWrapper").$(byText(gender)).click();
-                $("#userNumber").val(mobile);
-            });
-            step("Set date", () -> {
-                $("#dateOfBirthInput").clear();
-                $(".react-datepicker__month-select").selectOption(monthOfBirth);
-                $(".react-datepicker__year-select").selectOption(yearOfBirth);
-                $(".react-datepicker__day--0" + dayOfBirth).click();
-            });
-            step("Set subjects", () -> {
-                $("#subjectsInput").val(subject1);
-                $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
-                $("#subjectsInput").val(subject2);
-                $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
-            });
-            step("Set hobbies", () -> {
-                $("#hobbiesWrapper").$(byText(hobby1)).click();
-                $("#hobbiesWrapper").$(byText(hobby2)).click();
-                $("#hobbiesWrapper").$(byText(hobby3)).click();
-            });
+        step("Fill students registration form", () -> :
+            step("Fill common data", () -> :
+                $("#firstName").val(firstName)
+                $("#lastName").val(lastName)
+                $("#userEmail").val(email)
+                $("#genterWrapper").$(byText(gender)).click()
+                $("#userNumber").val(mobile)
+            )
+            step("Set date", () -> :
+                $("#dateOfBirthInput").clear()
+                $(".react-datepicker__month-select").selectOption(monthOfBirth)
+                $(".react-datepicker__year-select").selectOption(yearOfBirth)
+                $(".react-datepicker__day--0" + dayOfBirth).click()
+            )
+            step("Set subjects", () -> :
+                $("#subjectsInput").val(subject1)
+                $(".subjects-auto-complete__menu-list").$(byText(subject1)).click()
+                $("#subjectsInput").val(subject2)
+                $(".subjects-auto-complete__menu-list").$(byText(subject2)).click()
+            )
+            step("Set hobbies", () -> :
+                $("#hobbiesWrapper").$(byText(hobby1)).click()
+                $("#hobbiesWrapper").$(byText(hobby2)).click()
+                $("#hobbiesWrapper").$(byText(hobby3)).click()
+            )
             step("Upload image", () ->
-                    $("#uploadPicture").uploadFromClasspath("img/" + picture));
-            step("Set address", () -> {
-                $("#currentAddress").val(currentAddress);
-                $("#state").scrollTo().click();
-                $("#stateCity-wrapper").$(byText(state)).click();
-                $("#city").click();
-                $("#stateCity-wrapper").$(byText(city)).click();
-            });
+                    $("#uploadPicture").uploadFromClasspath("img/" + picture))
+            step("Set address", () -> :
+                $("#currentAddress").val(currentAddress)
+                $("#state").scrollTo().click()
+                $("#stateCity-wrapper").$(byText(state)).click()
+                $("#city").click()
+                $("#stateCity-wrapper").$(byText(city)).click()
+            )
             step("Submit form", () ->
-                    $("#submit").click());
-        });
+                    $("#submit").click())
+        )
 
         // TODO: refactor for cleaner test logic and less boilerplate ...
-        step("Verify successful form submit", () -> {
-            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
-            $x("//td[text()='Student Email']").parent().shouldHave(text(email));
-            $x("//td[text()='Gender']").parent().shouldHave(text(gender));
-            $x("//td[text()='Mobile']").parent().shouldHave(text(mobile));
-            $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
-            $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
-            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
-            $x("//td[text()='Picture']").parent().shouldHave(text(picture));
-            $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
-            $x("//td[text()='State and City']").parent().shouldHave(text(state + " error " + city));
-        });
-    }
-}
+        step("Verify successful form submit", () -> :
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"))
+            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName))
+            $x("//td[text()='Student Email']").parent().shouldHave(text(email))
+            $x("//td[text()='Gender']").parent().shouldHave(text(gender))
+            $x("//td[text()='Mobile']").parent().shouldHave(text(mobile))
+            $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth))
+            $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2))
+            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3))
+            $x("//td[text()='Picture']").parent().shouldHave(text(picture))
+            $x("//td[text()='Address']").parent().shouldHave(text(currentAddress))
+            $x("//td[text()='State and City']").parent().shouldHave(text(state + " error " + city))
+        )
+    
