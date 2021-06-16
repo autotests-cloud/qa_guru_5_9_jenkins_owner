5455be776b89f51cf0c69279dc000ac86c3fa7e8
Iakiv Kramarenko <yashaka@gmail.com>
2021-06-16 18:24:09 +0300

    before we finally start testing our changes, we need temporary comment some yet not translated java stuff...

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,53 +1,53 @@
-package tests;
-
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import config.Project;
-import helpers.AllureAttachments;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-
-public class ConfTest {
-
-    @BeforeAll
-    static void setup() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-
-        Configuration.browser = Project.config.browser();
-        System.out.println("remoteWebDriver: " + Project.config.remoteWebDriverUrl());
-
-        if(Project.config.remoteWebDriverUrl() != null) {
-            String user = Project.config.remoteWebDriverUser();
-            String password = Project.config.remoteWebDriverPassword();
-            Configuration.remote = String.format(Project.config.remoteWebDriverUrl(), user, password);
-
-            System.out.println("Running on a remote hub with...");
-            System.out.println("user: " + user);
-            System.out.println("password: " + password);
-            System.out.println("remote url: " + Project.config.remoteWebDriverUrl());
-            System.out.println("formatted url: " + String.format(Project.config.remoteWebDriverUrl(), user, password));
-        }
-    }
-
-    @AfterEach
-    void afterEach() {
-        AllureAttachments.addScreenshotAs("Last screenshot");
-        AllureAttachments.addPageSource();
-        AllureAttachments.addBrowserConsoleLogs();
-
-        if(Project.config.videoStorage() != null) {
-            AllureAttachments.addVideo();
-        }
-        
-        Selenide.closeWebDriver();
-    }
-}
+# package tests;
+#
+# import com.codeborne.selenide.Configuration;
+# import com.codeborne.selenide.Selenide;
+# import com.codeborne.selenide.logevents.SelenideLogger;
+# import config.Project;
+# import helpers.AllureAttachments;
+# import io.qameta.allure.selenide.AllureSelenide;
+# import org.junit.jupiter.api.AfterEach;
+# import org.junit.jupiter.api.BeforeAll;
+# import org.openqa.selenium.remote.DesiredCapabilities;
+#
+#
+# public class ConfTest {
+#
+#     @BeforeAll
+#     static void setup() {
+#         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+#
+#         DesiredCapabilities capabilities = new DesiredCapabilities();
+#         capabilities.setCapability("enableVNC", true);
+#         capabilities.setCapability("enableVideo", true);
+#         Configuration.browserCapabilities = capabilities;
+#
+#         Configuration.browser = Project.config.browser();
+#         System.out.println("remoteWebDriver: " + Project.config.remoteWebDriverUrl());
+#
+#         if(Project.config.remoteWebDriverUrl() != null) {
+#             String user = Project.config.remoteWebDriverUser();
+#             String password = Project.config.remoteWebDriverPassword();
+#             Configuration.remote = String.format(Project.config.remoteWebDriverUrl(), user, password);
+#
+#             System.out.println("Running on a remote hub with...");
+#             System.out.println("user: " + user);
+#             System.out.println("password: " + password);
+#             System.out.println("remote url: " + Project.config.remoteWebDriverUrl());
+#             System.out.println("formatted url: " + String.format(Project.config.remoteWebDriverUrl(), user, password));
+#         }
+#     }
+#
+#     @AfterEach
+#     void afterEach() {
+#         AllureAttachments.addScreenshotAs("Last screenshot");
+#         AllureAttachments.addPageSource();
+#         AllureAttachments.addBrowserConsoleLogs();
+#
+#         if(Project.config.videoStorage() != null) {
+#             AllureAttachments.addVideo();
+#         }
+#
+#         Selenide.closeWebDriver();
+#     }
+# }
